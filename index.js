f8c0a8af716f42bdd4daed1c22b30cc1766f304e
AlexFerRod86 <alexferrod86@gmail.com>
2025-02-03 10:45:39 +0100

    implementación de la lógica de una ronda.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getComputerChoice = function (i) {
     
     if (i<= 0.33){
         
-        return "tijeras"
+        return "tijera"
     } else if (i <= 0.66){
         
         return "piedra"
@@ -28,9 +28,46 @@ const getHumanCHoice = function(){
 
 } ;
 
+
+const playRound = function (choiceComputer,choiceHUman){
+
+    if (choiceHUman === "tijera" && choiceComputer === "piedra"){
+
+        return "La computadora gana"
+    }else if (choiceHUman === "tijera"  && choiceComputer === "papel"){
+
+        return "Felicidades has ganado esta ronda "
+    }else if(choiceHUman === "tijera" && choiceComputer === "tijera") {
+
+        return "Ohh habéis empatado nadie puntúa "
+    }else if (choiceHUman === "piedra" && choiceComputer === "papel"){
+
+        return "La computadora gana"
+    }else if (choiceHUman === "piedra"  && choiceComputer === "tijera"){
+
+        return "Felicidades has ganado esta ronda "
+    }else if(choiceHUman === "piedra" && choiceComputer === "piedra") {
+
+        return "Ohh habéis empatado nadie puntúa "
+    }if (choiceHUman === "papel" && choiceComputer === "tijera"){
+
+        return "La computadora gana"
+    }else if (choiceHUman === "papel"  && choiceComputer === "piedra"){
+
+        return "Felicidades has ganado esta ronda "
+    }else if(choiceHUman === "papel" && choiceComputer === "papel") {
+
+        return "Ohh habéis empatado nadie puntúa "
+    }
+
+
+}
+
 const choiceComputer = getComputerChoice(Math.random());
 const choiceHUman = getHumanCHoice().toLowerCase();
-
+const round = playRound(choiceComputer,choiceHUman);
 console.log(choiceComputer);
 console.log(choiceHUman);
+console.log(round);
+
 
