a178892c0681bec1d5dc85649b0f08ff8b163871
AlexFerRod86 <alexferrod86@gmail.com>
2025-02-05 14:53:47 +0100

    Implementada la lógica del juego completo de 5 rondas.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const getComputerChoice = function (i) {
     } else if (i <= 0.66){
         
         return "piedra"
-
+        
     }else if (i>=0.67){
         
         return "papel"
@@ -19,47 +19,47 @@ const getComputerChoice = function (i) {
 };
 
 const getHumanCHoice = function(){
-
+    
     let human = prompt ("ESCOGE ENTRE : PIEDRA PAPEL O TIJERA")
     
     return  human;
-
     
-
+    
+    
 } ;
 
 
 const playRound = function (choiceComputer,choiceHUman){
-
+    
     if (choiceHUman === "tijera" && choiceComputer === "piedra"){
-
+        
         return "La computadora gana"
     }else if (choiceHUman === "tijera"  && choiceComputer === "papel"){
-
+        
         return "Felicidades has ganado esta ronda "
     }else if(choiceHUman === "tijera" && choiceComputer === "tijera") {
-
+        
         return "Ohh habéis empatado nadie puntúa "
     }else if (choiceHUman === "piedra" && choiceComputer === "papel"){
 
         return "La computadora gana"
     }else if (choiceHUman === "piedra"  && choiceComputer === "tijera"){
-
+        
         return "Felicidades has ganado esta ronda "
     }else if(choiceHUman === "piedra" && choiceComputer === "piedra") {
-
+        
         return "Ohh habéis empatado nadie puntúa "
     }if (choiceHUman === "papel" && choiceComputer === "tijera"){
-
+        
         return "La computadora gana"
     }else if (choiceHUman === "papel"  && choiceComputer === "piedra"){
-
+        
         return "Felicidades has ganado esta ronda "
     }else if(choiceHUman === "papel" && choiceComputer === "papel") {
-
+        
         return "Ohh habéis empatado nadie puntúa "
     }
-
+    
 }
 
 const point = function(round ){
@@ -69,70 +69,37 @@ const point = function(round ){
         return scoreHuman = scoreHuman+1;   
 
     }else if (round === "La computadora gana") {
-
+        
         return scoreComputer = scoreComputer+1;
-
+        
     }
-
+    
 }
 
-const choiceComputer = getComputerChoice(Math.random());
-const choiceHUman = getHumanCHoice().toLowerCase();
-const round = playRound(choiceComputer,choiceHUman);
-const score = point(round);
-console.log(choiceComputer);
-console.log(choiceHUman);
-console.log(round);
-console.log(scoreComputer,scoreHuman);
-/*
-getComputerChoice(Math.random());
-getHumanCHoice().toLowerCase();
+const playGame = function (){
+    
+    for(let i = 0 ; i<=4;  i++){
+        
+        
+        const choiceComputer = getComputerChoice(Math.random());
+        const choiceHUman = getHumanCHoice().toLowerCase();
+        const round = playRound(choiceComputer,choiceHUman);
+        const score = point(round);
+        
+        
+        console.log(choiceHUman);
+        console.log(choiceComputer);
+        console.log(round);
+        console.log(scoreComputer,scoreHuman);
 
-playRound(choiceComputer,choiceHUman);
-point(round);
-
-console.log(scoreComputer,scoreHuman);
-
-getComputerChoice(Math.random());
-getHumanCHoice().toLowerCase();
-
-playRound(choiceComputer,choiceHUman);
-point(round);
-
-console.log(scoreComputer,scoreHuman);
-
-getComputerChoice(Math.random());
-getHumanCHoice().toLowerCase();
-
-playRound(choiceComputer,choiceHUman);
-point(round);
-
-console.log(scoreComputer,scoreHuman);
-
-getComputerChoice(Math.random());
-getHumanCHoice().toLowerCase();
-
-playRound(choiceComputer,choiceHUman);
-point(round);
-
-console.log(scoreComputer,scoreHuman);
-
-getComputerChoice(Math.random());
-getHumanCHoice().toLowerCase();
-
-playRound(choiceComputer,choiceHUman);
-point(round);
-
-
-console.log(scoreComputer,scoreHuman);*/
+    }
+}
 
 
 
 
+const game= playGame(1);
 
 
-
-
-
-
+console.log(game);
 
